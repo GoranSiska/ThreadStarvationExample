adeb041ee603d6649bd89fd6da8514b9739427d3
Goran Siska <goran.siska@gmail.com>
2022-09-05 07:14:34 +0200

    Add project files.

---
+++ TS/Controllers/WeatherForecastController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        async Task<bool> PretendQueryCustomerFromDbAsync()
+        {
+            // To keep the demo app easy to set up and performing consistently we have replaced a real database query
+            // with a fixed delay of 500ms. The impact on application performance should be similar to using a real
+            // database that had similar latency.
+            await Task.Delay(1000);
+            return true;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            bool result = false;
+            //Task dbTask = Task.Delay(1000);
+            //while (!dbTask.IsCompleted)
+            //{
+            //    Thread.Sleep(10);
+            //    result = true;
+            //}
+            //dbTask.Wait();
+
+            result = PretendQueryCustomerFromDbAsync().Result;
+
+            //result = Task.Run(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    return true;
+            //}).Result;
+
+            //await Task.Yield();
+            //var result = Task.Factory.StartNew(() => {
+            //    Thread.Sleep(1000);
+            //    return true;
+            //}).Result;
+
+            //await Task.Delay(1000);
+
+            //result = true;
+
+            if (result)
+            {
+                var rng = new Random();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            } 
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ TSClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TSClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(@"https://localhost:44343");
+
+            Console.WriteLine("Press S to start");
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey().Key;
+                    if (key == ConsoleKey.S)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey().Key;
+                    if (key == ConsoleKey.B)
+                    {
+                        break;
+                    }
+                }
+                _ = Task.Run(async () =>
+                {
+                    //while (!cancellationTokenSource.Token.IsCancellationRequested)
+                    //{
+                    Console.Write("s");
+                    client.GetAsync("weatherforecast").ContinueWith(async (m) =>
+                    {
+                        if (m.Exception != null)
+                        {
+                            Console.WriteLine(m.Exception);
+                        }
+                        else
+                        {
+                            var r = await m;
+                            var c = await r.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            //Console.WriteLine(c);
+                            Console.Write("r");
+                        }
+                    });
+                    //Console.WriteLine("calc");
+                    //count++;
+                    //var diff = DateTime.Now - start;
+                    //Console.WriteLine(count / diff.TotalSeconds);
+                    //Thread.Sleep(10000);
+                    //}
+                });
+
+                Thread.Sleep(50);
+            }
+
+        }
+    }
+}
